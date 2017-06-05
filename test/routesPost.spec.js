0fadd85e8a38a71f5870e813def9df8a30176956
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-06-05 14:09:39 -0600

    Change before and after hook in POST - was failing but now working

--- test/routesPost.spec.js
+++ test/routesPost.spec.js
@@ -28,144 +28,145 @@ describe('POST /api/v1/names', () => {
       done()
     })
   })
+  describe('Post Routes', () => {
+    it('should post a new name', (done) => {
+      chai.request(server)
+      .post('/api/v1/names')
+      .send(
+        {
+          name: 'RobbieCool',
+          gender: 'M',
+          count: 1,
+          year: 1880,
+        })
+        .set('Authorization', process.env.TOKEN)
+        .end((error, response) => {
+          response.should.have.status(201)
+          chai.request(server)
+          .get('/api/v1/names?name=RobbieCool')
+          .end((err, res) => {
+            res.status.should.equal(200)
+            res.body[0][0].should.have.property('count')
+            res.body[0][0].should.have.property('name')
+            res.body[0][0].should.have.property('year')
+            res.body[0][0].should.have.property('gender')
+            res.body[0][0].count.should.equal(1)
+            res.body[0][0].name.should.equal('RobbieCool')
+            res.body[0][0].gender.should.equal('M')
+            res.body[0][0].year.should.equal(1880)
+            done()
+          })
+        })
+    })
 
-  it('should post a new name', (done) => {
-    chai.request(server)
-    .post('/api/v1/names')
-    .send(
-      {
-        name: 'RobbieCool',
-        gender: 'M',
-        count: 1,
-        year: 1880,
-      })
-      .set('Authorization', process.env.TOKEN)
-      .end((error, response) => {
-        response.should.have.status(201)
-        chai.request(server)
-        .get('/api/v1/names?name=RobbieCool')
-        .end((err, res) => {
-          res.status.should.equal(200)
-          res.body[0][0].should.have.property('count')
-          res.body[0][0].should.have.property('name')
-          res.body[0][0].should.have.property('year')
-          res.body[0][0].should.have.property('gender')
-          res.body[0][0].count.should.equal(1)
-          res.body[0][0].name.should.equal('RobbieCool')
-          res.body[0][0].gender.should.equal('M')
-          res.body[0][0].year.should.equal(1880)
+    it('should not post a new name if missing year', (done) => {
+      chai.request(server)
+      .post('/api/v1/names')
+      .send(
+        {
+          name: 'RobbieCool',
+          gender: 'M',
+          count: 1,
+        })
+        .set('Authorization', process.env.TOKEN)
+        .end((error, response) => {
+          response.should.have.status(404)
           done()
         })
-      })
-  })
-
-  it('should not post a new name if missing year', (done) => {
-    chai.request(server)
-    .post('/api/v1/names')
-    .send(
-      {
-        name: 'RobbieCool',
-        gender: 'M',
-        count: 1,
-      })
-      .set('Authorization', process.env.TOKEN)
-      .end((error, response) => {
-        response.should.have.status(404)
-        done()
-      })
-  })
-
-  it('should not post a new name if missing count', (done) => {
-    chai.request(server)
-    .post('/api/v1/names')
-    .send(
-      {
-        name: 'RobbieCool',
-        gender: 'M',
-        year: 1880,
-      })
-      .set('Authorization', process.env.TOKEN)
-      .end((error, response) => {
-        response.should.have.status(404)
-        done()
-      })
-  })
-
-  it('should not post a new name if missing gender', (done) => {
-    chai.request(server)
-    .post('/api/v1/names')
-    .send(
-      {
-        name: 'RobbieCool',
-        count: 1,
-        year: 1880,
-      })
-      .set('Authorization', process.env.TOKEN)
-      .end((error, response) => {
-        response.should.have.status(404)
-        done()
-      })
-  })
-
-  it('should not post a new name if missing name', (done) => {
-    chai.request(server)
-    .post('/api/v1/names')
-    .send(
-      {
-        gender: 'M',
-        count: 1,
-        year: 1880,
-      })
-      .set('Authorization', process.env.TOKEN)
-      .end((error, response) => {
-        response.should.have.status(404)
-        done()
-      })
-  })
-
-  it('should not post a new name not depending on data type', (done) => {
-    chai.request(server)
-    .post('/api/v1/names')
-    .send(
-      {
-        gender: 'M',
-        count: '1',
-        year: '1880',
-      })
-      .set('Authorization', process.env.TOKEN)
-      .end((error, response) => {
-        response.should.have.status(404)
-        done()
-      })
-  })
-
-  it('should return 404 for a non existent route', (done) => {
-    chai.request(server)
-    .post('/api/v1/naem')
-    .set('Authorization', process.env.TOKEN)
-    .end((error, response) => {
-      error.response.should.have.status(404)
-      done()
     })
-  })
 
-  it('should return 404 if nothing send', (done) => {
-    chai.request(server)
-    .post('/api/v1/name')
-    .send({})
-    .end((error, response) => {
-      error.response.should.have.status(404)
-      done()
+    it('should not post a new name if missing count', (done) => {
+      chai.request(server)
+      .post('/api/v1/names')
+      .send(
+        {
+          name: 'RobbieCool',
+          gender: 'M',
+          year: 1880,
+        })
+        .set('Authorization', process.env.TOKEN)
+        .end((error, response) => {
+          response.should.have.status(404)
+          done()
+        })
     })
-  })
 
-  it('should not create if unauthorized', (done) => {
-    chai.request(server)
-    .post('/api/v1/names')
-    .send({})
-    .end((err, response) => {
-      response.should.have.status(403)
-      done()
+    it('should not post a new name if missing gender', (done) => {
+      chai.request(server)
+      .post('/api/v1/names')
+      .send(
+        {
+          name: 'RobbieCool',
+          count: 1,
+          year: 1880,
+        })
+        .set('Authorization', process.env.TOKEN)
+        .end((error, response) => {
+          response.should.have.status(404)
+          done()
+        })
+    })
+
+    it('should not post a new name if missing name', (done) => {
+      chai.request(server)
+      .post('/api/v1/names')
+      .send(
+        {
+          gender: 'M',
+          count: 1,
+          year: 1880,
+        })
+        .set('Authorization', process.env.TOKEN)
+        .end((error, response) => {
+          response.should.have.status(404)
+          done()
+        })
+    })
+
+    it('should not post a new name not depending on data type', (done) => {
+      chai.request(server)
+      .post('/api/v1/names')
+      .send(
+        {
+          gender: 'M',
+          count: '1',
+          year: '1880',
+        })
+        .set('Authorization', process.env.TOKEN)
+        .end((error, response) => {
+          response.should.have.status(404)
+          done()
+        })
+    })
+
+    it('should return 404 for a non existent route', (done) => {
+      chai.request(server)
+      .post('/api/v1/naem')
+      .set('Authorization', process.env.TOKEN)
+      .end((error, response) => {
+        error.response.should.have.status(404)
+        done()
+      })
+    })
+
+    it('should return 404 if nothing send', (done) => {
+      chai.request(server)
+      .post('/api/v1/name')
+      .send({})
+      .end((error, response) => {
+        error.response.should.have.status(404)
+        done()
+      })
+    })
+
+    it('should not create if unauthorized', (done) => {
+      chai.request(server)
+      .post('/api/v1/names')
+      .send({})
+      .end((err, response) => {
+        response.should.have.status(403)
+        done()
+      })
     })
   })
 })
